dbf6680a763ad4b986a1c2d3d52f99007c65f90e
RogerTM <roger213tm@gmail.com>
2017-07-02 01:39:30 -0400

    Extructurado el sitio, será un landing page, muy sencillo y moderno...

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Twenty'em All
+ *
+ * @package			WordPress
+ * @subpackage		Twenty'em All
+ * @author			RogerTM
+ * @license			license.txt
+ * @link			https://twenty-em.themingisprose.com
+ * @since 			Twenty'em All 1.0
+ */
+
+/**
+ * Twenty'em All files
+ *
+ * @since Twenty'em All 1.0
+ */
+require( get_stylesheet_directory() . '/functions/index.php' );
+?>
