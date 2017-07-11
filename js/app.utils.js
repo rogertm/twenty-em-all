feb738da343f44ade5e5f7e7562cb792667c75ec
RogerTM <roger213tm@gmail.com>
2017-07-11 19:43:25 -0400

    uglifyjs app.utils.js

--- js/app.utils.js
+++ js/app.utils.js
@@ -23,6 +23,29 @@ jQuery(document).ready(function($) {
 	}
 	t_em_all_github_panel();
 
+	// make code pretty
+	$(function(){
+		var $window = $(window)
+		window.prettyPrint && prettyPrint()
+	});
+	$('.prettyprint').addClass('linenums');
+	$('.prettyprint.nolinenums').removeClass('linenums');
+
+
+	// Move title attr to rel attr
+	$(function(){
+		$('pre').each(function(index){
+			var $pre	= $(this);
+			var $title	= ( $pre.prop('title') ) ? $pre.prop('title') : 'code';
+			$pre.attr({
+				'rel':$title
+			});
+			$pre.removeAttr('title');
+		});
+	});
+
+	$('.comment-body pre').wrapInner('<code></code>');
+
 	$(window).resize(function(){
 		t_em_all_github_panel();
 	});
